8db8ab202611b518fac31c7ee1c9167cad1014e8
Claire Whitten <claire.whitten@student.kdg.be>
2021-02-22 16:52:22 +0100

    Lesson 1 - Task Console write, read and strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,7 +14,48 @@ namespace HelloWorld
         /*If you type Console. the console properties and methods come up in a list
         To github - add to source control*/
 
-        { /*The code block
+        {//The code block
+                     string food;
+
+        Console.WriteLine("What would you like to order?");
+            food = Console.ReadLine();
+
+            // Method 1
+            Console.Write("Sure "); //write means next piece of text will be beside it 
+            Console.Write(food);
+            Console.WriteLine(" coming up! "); // writeLine means next piece of text will be below it
+
+
+            //Method 2
+            Console.WriteLine("Sure " + food + " coming up!");
+
+
+           
+            Console.WriteLine("What would you like to drink?");
+            string drink;
+        drink = Console.ReadLine();
+           
+            
+            //method 3
+            Console.WriteLine($"Ok, so that's {food} and {drink}. No problem. Your order is on its way");
+
+            //method 4
+            Console.WriteLine("Ok, so that's {0} and {1}. No problem. Your order is on its way", food, drink);
+
+           
+            //Method 5
+
+            string begin = "Ok, so that's ";
+        string middle = " and ";
+        string end = ". Your order is on its way";
+        Console.WriteLine(begin + food + middle + drink + end);
+
+            Console.ReadLine();  // This ReadLine ensures the console stays open and doesn't close because pc waiting for users input 
+
+                   
+                    
+                   
+            /*
             Console.WriteLine("Hello World"); //class green, method yellow
             Console.WriteLine("Wie ben je?");
 
@@ -46,35 +87,7 @@ namespace HelloWorld
 
 
 
-            string food;
-            
-         
 
-            Console.WriteLine("What would you like to order?");
-            food = Console.ReadLine();
-            Console.Write("Sure ");
-            Console.Write(food);
-            Console.Write(" coming up! ");
-
-
-
-            Console.WriteLine("Sure " + food + " coming up!");
-
-            Console.WriteLine("What would you like to drink?");
-            string drink;
-            drink = Console.ReadLine();
-            Console.WriteLine($"Ok, so that's {food} and {drink}. No problem. Your order is on its way");
-
-          
-            Console.WriteLine("Ok, so that's {0} and {1}. No problem. Your order is on its way", food, drink);
-
-            Console.WriteLine("Ok, so that's {0} and {1}. No problem. Your order is on its way", food, drink);
-
-
-            string begin = "Ok, so that's ";
-            string middle = " and ";
-            string end = ". Your order is on its way";
-            Console.WriteLine(begin + food + middle + drink + end);
 
 
 
