ad123177a25c5ba6bcda5ecac89ee8763d18a554
Claire Whitten <claire.whitten@student.kdg.be>
2021-02-22 14:01:26 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+//using a packet - so you can write less code  
+//- Console.readLine, writeLine belongs to this packet (System.Console.WriteLine)
+
+
+namespace HelloWorld 
+    //don't change this - name of project 
+    //if you change the name doesn't change elsewhere
+{
+    class Program //class needed for static voidpackets with code in the 
+    {
+        static void Main(string[] args) //static void to call a method
+        
+        { //The code block
+            Console.WriteLine("Hello World"); //class green, method yellow
+            Console.ReadLine();
+            //If you type Console. the console properties and methods come up in a list
+            //To github - add to source control
+        }
+    }
+}
